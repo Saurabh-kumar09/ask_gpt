db4aa319e6fc9bf865404efa7d25365a81f32fc4
Saurabh Kumar <saurabh09.ind@gmail.com>
2024-01-19 22:29:03 +0530

    first commit

---
+++ lib/chat_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+        const Expanded(
+          child: TextField(
+            decoration: InputDecoration.collapsed(hintText: "Send a message"),
+          ),
+        ),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.send))
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("AskGPT")),
+        body: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(color: context.cardColor),
+              child: _buildTextComposer(),
+            )
+          ],
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:ask_gpt/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT Demo',
+      theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
+      home: ChatScreen(),
+    );
+  }
+}
