61a4840d8bc6b588b81659ca9e8fd70a4ed753cf
Saurabh Kumar <saurabh09.ind@gmail.com>
2024-01-26 02:23:51 +0530

    chatMessage.dart changes

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class ChatMessage extends StatelessWidget {
   const ChatMessage({super.key, required this.text, required this.sender});
@@ -10,27 +11,17 @@ class ChatMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          margin: const EdgeInsets.only(right: 16.0),
-          child: CircleAvatar(
-            child: Text(sender[0]),
-          ),
-        ),
-        Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              sender,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 5.0),
-              child: Text(text),
-            )
-          ],
-        ))
+        Text(sender)
+            .text
+            .make()
+            .box
+            .color(sender == "user" ? Vx.red200 : Vx.green200)
+            .p16
+            .rounded
+            .alignCenter
+            .makeCentered(),
+        Expanded(child: text.trim().text.bodyText1(context).make().px8())
       ],
-    );
+    ).py8();
   }
 }
